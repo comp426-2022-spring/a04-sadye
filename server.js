5b20ace5bb1c9f345d86f89b845a1ea09d3dba07
sadye <sadye@live.unc.edu>
2022-04-11 17:09:32 -0400

    im going crazy

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
   let logdata = {
     remoteaddr: req.ip,
     remoteuser: req.user,
-    time: Date.now(),
+    datetime: Date.now(),
     method: req.method,
     url: req.url,
     protocol: req.protocol,
