6896caed6a6c3ccf419699a72582d7421153f11e
sadye <sadye@live.unc.edu>
2022-04-12 10:11:59 -0400

    removed sevure

--- server.js
+++ server.js
@@ -60,17 +60,20 @@ app.use((req, res, next) => {
     url: req.url,
     protocol: req.protocol,
     httpversion: req.httpVersion,
-    secure: req.secure,
     status: res.statusCode,
     referer: req.headers['referer'],
     useragent: req.headers['user-agent']
   }
+
+
+
   const stmt = database.prepare(`INSERT INTO accesslog (remoteaddr, 
-    remoteuser, time, method, url, protocol, httpversion, secure, 
-    status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`)
+    remoteuser, time, method, url, protocol, httpversion, 
+    status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`)
+
 
   const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, 
-    logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.secure, 
+    logdata.method, logdata.url, logdata.protocol, logdata.httpversion,
     logdata.status, logdata.referer, logdata.useragent)
   next()
   })
